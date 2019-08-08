ac7db76b82cf1e5752160855286ef38a3846564b
Penylove <nyamadiprince@hotmail.com>
2019-08-08 01:11:20 +0000

    initial django files

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
+from . import views
+
+
+urlpatterns = [
+    path('',views.bloghome,  name='blog-home'),
+   
+]
